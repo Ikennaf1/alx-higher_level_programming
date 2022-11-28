6288e6bd023ee993e6bcee64b6cec7f01bfc7344
Ikennaf1 <iiikbros202@gmail.com>
2022-11-28 23:48:56 +0100

    Fix for task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,23 @@
 int check_cycle(listint_t *list)
 {
 	int result = 0;
-	listint_t *temp = list;
+	listint_t *temp = list, *fast_temp = list;
 
-	while (temp->next != NULL)
+	if (list == NULL)
+		return (0);
+
+	while ((temp && fast_temp) && fast_temp->next)
 	{
-		if (temp->next == list)
+		temp = temp->next;
+		if (fast_temp->next || fast_temp->next->next)
+			fast_temp = fast_temp->next;
+		else
+			break;
+		if (temp == fast_temp)
 		{
 			result = 1;
 			break;
 		}
-		temp = temp->next;
 	}
 	return (result);
 }
