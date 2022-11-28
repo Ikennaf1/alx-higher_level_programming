ad60cc95ac3c35a42952e4c066eb9ac519e29b29
Ikennaf1 <iiikbros202@gmail.com>
2022-11-28 20:51:55 +0100

    Initial commit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a list has a cycle
+ * @list: The given list
+ * Return: 1 or 0
+ */
+int check_cycle(listint_t *list)
+{
+	int result = 0;
+	listint_t *temp = list;
+
+	while (temp.next != NULL)
+	{
+		if (temp.next == list)
+		{
+			result = 1;
+			break;
+		}
+		temp = temp.next;
+	}
+	return (result);
+}
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int check_cycle(listint_t *list);
+int _putchar(char s);
+
+#endif /* _LISTS_ */
