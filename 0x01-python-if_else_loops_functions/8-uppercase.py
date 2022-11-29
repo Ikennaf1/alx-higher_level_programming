5f62df0c1c52af7ddf565ed44dcd3c54345b40ff
Ikennaf1 <iiikbros202@gmail.com>
2022-11-29 20:57:01 +0100

    Fix task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(s):
     endline = ""
     if s == "":
-        s = "\n"
+        s = "\0"
     for i in range(0, len(s)):
         endline = "" if i < len(s) - 1 else "\n"
         if ord(s[i]) >= 97 and ord(s[i]) <= 122:
