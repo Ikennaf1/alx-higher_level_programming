f0766666a070a3516ffe9280fc2b27f7a0f9a7e5
Ikennaf1 <iiikbros202@gmail.com>
2022-12-07 19:49:28 +0100

    Fix task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 def multiply_by_2(a_dictionary):
     if a_dictionary:
         new_dictionary = a_dictionary.copy()
-        for x in new_dictionary:
+        for x in range(len(new_dictionary)):
             new_dictionary[x] = new_dictionary[x] * 2
     return new_dictionary
