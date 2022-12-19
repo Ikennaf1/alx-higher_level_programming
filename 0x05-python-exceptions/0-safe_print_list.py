1513daa05a93445bb4a93e0fab3952b714150a8f
Ikennaf1 <iiikbros202@gmail.com>
2022-12-19 15:29:44 +0100

    Initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    endline = ""
+    try:
+        for i in range(0, x):
+            endline = "" if i < (x - 1) else "\n"
+            print("{}".format(my_list[i]), end=endline)
+    except IndexError:
+        print()
+        return i
+    i += 1
+    return i
