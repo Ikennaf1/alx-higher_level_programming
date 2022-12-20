65797599a27f3b3d98e392e3e1125f279652c0c7
Ikennaf1 <iiikbros202@gmail.com>
2022-12-20 14:16:21 +0100

    Added docs to task 2, 3

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
+"""Square Class
+
+A square class that does a lot more
+
+"""
+
+
 class Square:
-    """Square Class
-
-    A Square Class
-
-    """
 
     def __init__(self, size=0):
         """__init__
