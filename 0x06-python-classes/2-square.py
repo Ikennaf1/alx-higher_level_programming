bceeebf2013e3adb89532232dfe7592805c9eea5
Ikennaf1 <iiikbros202@gmail.com>
2022-12-20 14:11:08 +0100

    Added docs to task 2, 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 """Square Class
 
-Square class that raises exceptions
+A Square Class
 
 """
 
@@ -12,20 +11,22 @@ class Square:
     def __init__(self, size=0):
         """__init__
 
-        The __init__ method initializes the size value
-        of the square.
+        The __init__ method initializes the size value of the square.
 
         Attributes:
-            size (int): The size of the square.
+            size (:obj:`int`, optional): The size of the square.
 
         Raises:
-            TypeError: If size `type` is not `int`
+            TypeError: If `size` type is not `int`.
 
-            ValueError: If `size` is negative
+            ValueError: If `size` is less than `0`.
 
         """
+
         if type(size) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
+
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
+
         self.__size = size
