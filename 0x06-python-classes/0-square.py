e58707536f47ac3bbff420691dc116262ff4d605
Ikennaf1 <iiikbros202@gmail.com>
2022-12-20 13:47:14 +0100

    Added docs to task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 class Square:
+    """Square Class
+
+    A Square Class
+
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
 class Square:
+    """Square Class
+
+    A Square Class
+
+    """
+
     def __init__(self, size=None):
+        """__init__
+        Intializez the size of the square.
+
+        Attributes:
+            size (:obj: `int`, optional): The size of the square.
+
+        """
         self.__size = size
