77287afacdf0dfdd4d79d2cda49615f88507db09
Ikennaf1 <iiikbros202@gmail.com>
2023-01-09 16:29:25 +0100

    hmmm

--- 0x0A-python_inheritance/0-lookup.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def lookup(obj):
-    """
-    Returns list of available attributes and methods
-    """
-    return dir(obj)
--- 0x0A-python_inheritance/4-inherits_from.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def inherits_from(obj, a_class):
-    """
-    Checks if object is instance of a_class class
-    """
-    return isinstance(obj, a_class) and not issubclass(a_class, obj.__class__)
