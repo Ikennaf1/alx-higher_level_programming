b2127ea5ccf1c83525915491af7305f89cedc455
Ikennaf1 <iiikbros202@gmail.com>
2023-01-10 15:03:43 +0100

    7 fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
+"""
+A python script
+"""
+
+
 from os import path
 from sys import argv
+
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
