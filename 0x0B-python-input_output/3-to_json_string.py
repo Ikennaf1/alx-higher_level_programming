7a271a76fd36d35f10cd8f92bfeb044c73c33641
Ikennaf1 <iiikbros202@gmail.com>
2023-01-10 12:40:55 +0100

    Tasks 3-4

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+A python module
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    Returns JSON erpresentation of an object
+    """
+    return json.dumps(my_obj)
+
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+A python module
+"""
+
+
+import json
+def from_json_string(my_str):
+    """
+    Converts from json to object
+    """
+    return json.loads(my_str)
